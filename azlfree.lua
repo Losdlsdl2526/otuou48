d021cde51e3f6b9f4c9270c84ccabed22ed7ae40
Losdlsdl2526 <aeroon923@gmail.com>
2026-06-27 21:05:50 +0500

    azlfree.lua

---
+++ azlfree.lua
@@ -0,0 +1,6 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+if player then
+    player:Kick("This script does not have a free version yet. Join the Discord server to buy it https://discord.gg/eQSp3kqj3Y")
+end
